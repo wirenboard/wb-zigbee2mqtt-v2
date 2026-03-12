7c16e69935d1280d6744dfba47aeb3fe8cdbcba3
victor.fedorov <victor.fedorov@wirenboard.com>
2026-03-12 10:46:06 +0300

    refactor: move package to wb namespace, rename wb subpackage to wb_converter

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     maintainer_email="info@wirenboard.com",
     description="Wiren Board Zigbee2MQTT bridge v2",
     url="https://github.com/wirenboard/wb-zigbee2mqtt-v2",
-    packages=["wb_zigbee2mqtt", "wb_zigbee2mqtt.z2m", "wb_zigbee2mqtt.wb"],
+    packages=["wb.zigbee2mqtt", "wb.zigbee2mqtt.z2m", "wb.zigbee2mqtt.wb_converter"],
     scripts=["bin/wb-zigbee2mqtt"],
     license="MIT",
 )
