497181c64857f20bf3582e7cd669b6cc6568feb4
victor.fedorov <victor.fedorov@wirenboard.com>
2026-03-16 12:32:23 +0300

    refactor: change controls orders

--- wb/zigbee2mqtt/wb_converter/controls.py
+++ wb/zigbee2mqtt/wb_converter/controls.py
@@ -35,36 +35,36 @@ BRIDGE_CONTROLS: dict[str, ControlMeta] = {
         type="text", readonly=True, order=2,
         title={"en": "Version", "ru": "Версия"},
     ),
-    BridgeControl.LOG_LEVEL: ControlMeta(
-        type="text", readonly=True, order=3,
-        title={"en": "Log level", "ru": "Уровень логов"},
-    ),
-    BridgeControl.LOG: ControlMeta(
-        type="text", readonly=True, order=4,
-        title={"en": "Log", "ru": "Лог"},
-    ),
     BridgeControl.PERMIT_JOIN: ControlMeta(
-        type="switch", readonly=False, order=5,
+        type="switch", readonly=False, order=3,
         title={"en": "Permit join", "ru": "Разрешить подключение"},
     ),
     BridgeControl.DEVICE_COUNT: ControlMeta(
-        type="value", readonly=True, order=6,
+        type="value", readonly=True, order=4,
         title={"en": "Device count", "ru": "Количество устройств"},
     ),
     BridgeControl.LAST_JOINED: ControlMeta(
-        type="text", readonly=True, order=7,
-        title={"en": "Last joined", "ru": "Последнее подключенное"},
+        type="text", readonly=True, order=5,
+        title={"en": "Last joined", "ru": "Последнее сопряженное"},
     ),
     BridgeControl.LAST_LEFT: ControlMeta(
-        type="text", readonly=True, order=8,
+        type="text", readonly=True, order=6,
         title={"en": "Last left", "ru": "Последнее вышедшее из сети"},
     ),
     BridgeControl.LAST_REMOVED: ControlMeta(
-        type="text", readonly=True, order=9,
+        type="text", readonly=True, order=7,
         title={"en": "Last removed", "ru": "Последнее удаленное"},
     ),
     BridgeControl.UPDATE_DEVICES: ControlMeta(
-        type="pushbutton", readonly=False, order=10,
+        type="pushbutton", readonly=False, order=8,
         title={"en": "Update devices", "ru": "Обновить устройства"},
+    ),
+    BridgeControl.LOG_LEVEL: ControlMeta(
+        type="text", readonly=True, order=9,
+        title={"en": "Log level", "ru": "Уровень логов"},
+    ),
+    BridgeControl.LOG: ControlMeta(
+        type="text", readonly=True, order=10,
+        title={"en": "Log", "ru": "Лог"},
     ),
 }
